75ffeadc7d5a349c64a9bc9049d7964e675faf56
elepampam <budisuryadarma0407@gmail.com>
2018-04-27 10:54:09 +0800

    update for deploy

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import Lottie from 'react-lottie';
+// import Lottie from 'react-lottie';cls
 import styled from 'styled-components';
 import * as CryingAnimation from '../utils/animation/crying.json'
 
@@ -19,20 +19,20 @@ export default class FourZeroFour extends Component{
 		super()		
 	}
 	render(){
-		const cryingAnimOptions = {
-			loop: true,
-			autoplay: true, 
-			animationData: CryingAnimation,
-		}
+	// 	const cryingAnimOptions = {
+	// 		loop: true,
+	// 		autoplay: true, 
+	// 		animationData: CryingAnimation,
+	// 	}
+	// 	<Lottie 
+	// 	options={cryingAnimOptions}
+	// 	height={200}
+	// 	width={200}
+	//   />
 		return(
 			<section className="section">
 				<div className="container">
 					<BigAttention>OH MY GOSH! YOU HAVE REACHED HERE!</BigAttention>
-					<Lottie 
-						options={cryingAnimOptions}
-						height={200}
-						width={200}
-		          	/>
 		          	<SmallAttention>
 		          		Looks like the page you are looking doesn't exist.<br/>
 		          		Please Check the URL and try again.
